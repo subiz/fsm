c0d24299aaae93882e472ce9be1cb33390ae0b54
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2018-01-25 18:37:11 +0700

    add fsm_test

---
+++ fsm_test.go
@@ -0,0 +1,26 @@
+package fsm_test
+
+import (
+	"testing"
+	"bitbucket.org/subiz/fsm"
+)
+
+var st fsm.FSM = fsm.New()
+
+type S struct {}
+
+func (s S) State1(e string, ps []interface{}) (string, []interface{}) {
+	return "state2", nil
+}
+
+func (s S) State2(e string, ps []interface{}) (string, []interface{}) {
+	st.Stop()
+	return "", []interface{}{4}
+}
+
+func TestFSM(t *testing.T) {
+	s := &S{}
+	st.Map("init", s.State1)
+	st.Map("state2", s.State2)
+	st.Run("init", nil)
+}
